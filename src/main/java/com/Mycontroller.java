ea4c8889397fb23bddad34c4a709e48214bb51ce
shahu shivaji kumbhar <shahuk1042004@gmail.com>
2026-05-26 18:28:45 +0530

    Change request mapping from '/hu' to '/hello'

--- src/main/java/com/Mycontroller.java
+++ src/main/java/com/Mycontroller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Mycontroller {
 	
-	@RequestMapping("/hu")
+	@RequestMapping("/hello")
 	public String view() {
 		return "home";
 	}
